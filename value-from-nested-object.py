23a299da6a2254523be23924de03d22d8829e2e0
root <root@python.uh01sy4q0cfufbs0dl0j30pdnh.bx.internal.cloudapp.net>
2023-04-28 17:43:06 +0000

    Added the file

---
+++ value-from-nested-object.py
@@ -0,0 +1,26 @@
+import sys
+
+def get_value(obj, key):
+    keys = key.split('/')
+    value = obj
+    try:
+        for k in keys:
+            value = value[k]
+        return value
+    except (KeyError, TypeError):
+        return None
+
+# Check if command-line arguments are provided
+if len(sys.argv) >= 3:
+    object_str = sys.argv[1]
+    key = sys.argv[2]
+
+    try:
+        object = eval(object_str)
+        value = get_value(object, key)
+        print(value)
+    except (NameError, SyntaxError):
+        print("Invalid object format.")
+else:
+    print("Usage: python script.py <object> <key>")
+
