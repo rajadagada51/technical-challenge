365461ba1b69d690482df89b2928b80e519686a1
rajadagada51 <97170585+rajadagada51@users.noreply.github.com>
2023-05-01 15:52:59 +0530

    Update main.tf

--- terraform/modules/postgresql/main.tf
+++ terraform/modules/postgresql/main.tf
@@ -14,8 +14,7 @@ resource "azurerm_postgresql_server" "postgres_server" {
   location            = var.location
   resource_group_name = var.resource_group_name
   sku_name            = var.pg_sku_name
-  storage_mb          = var.pg_storage_mb
   version             = var.pg_version
-  administrator_login = var.pg_username
+  administrator_login = "${var.product_name}${var.environment}${var.location}"
   administrator_login_password = random_password.pg_passwd.result
 }
