38101de3687c8175e593a76e7ec76d225ad36eca
rajadagada51 <97170585+rajadagada51@users.noreply.github.com>
2023-05-01 15:41:11 +0530

    Create variable.tf

---
+++ terraform/modules/postgresql/variable.tf
@@ -0,0 +1,13 @@
+variable "resource_group_name" {}
+
+variable "location" {}
+
+variable "product_name" {}
+
+variable "sku_name" {}
+
+variable "storage_mb" {}
+
+variable "postgres_version" {}
+
+variable "username" {} 
